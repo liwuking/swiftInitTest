2e38cff331bae2a2b52cbb2f2fc3cc1796dab6b9
liwuyang <liwuyang@lbesec.com>
2018-05-04 21:15:19 +0800

    Initial Commit

---
+++ swiftInitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftInitTest
+//
+//  Created by lbe on 2018/5/4.
+//  Copyright © 2018年 liwuyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
